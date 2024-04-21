dbbc6559023b452552b3b1c84ec538739685a9fc
Marcelo Vaz Castilho <marcelo.vaz.castilho@gmail.com>
2024-04-21 18:34:07 -0300

    feat: add pagina de checkout com stripe

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -2,12 +2,115 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Models\Address;
+use App\Models\Order;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 
+#[Title('Checkout Page - E-commerce')]
 class CheckoutPage extends Component
 {
+    public $first_name;
+    public $last_name;
+    public $phone;
+    public $street_address;
+    public $city;
+    public $state;
+    public $zip_code;
+    public $payment_method;
+
+    public function mount()
+    {
+        $cartItems = CartManagement::getCartItemsFromCookie();
+        if(count($cartItems) == 0){
+            return redirect('/products');
+        }
+    }
+
+    public function placeOrder()
+    {
+        $this->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'phone' => 'required',
+            'street_address' => 'required',
+            'city' => 'required',
+            'state' => 'required',
+            'zip_code' => 'required',
+            'payment_method' => 'required',
+        ]);
+
+        $cartItems = CartManagement::getCartItemsFromCookie();
+
+        $lineItems = [];
+        foreach ($cartItems as $item) {
+            $lineItems[] = [
+                'price_data' => [
+                    'currency' => 'brl',
+                    'unit_amount' => $item['unit_amount'] * 100,
+                    'product_data' => [
+                        'name' => $item['name'],
+                    ],
+                ],
+                'quantity' => $item['quantity'],
+            ];
+        }
+
+        $order = new Order();
+        $order->user_id = auth()->user()->id;
+        $order->grand_total = CartManagement::calculateGrandTotal($cartItems);
+        $order->payment_method = $this->payment_method;
+        $order->payment_status = 'pending';
+        $order->status = 'new';
+        $order->currency = 'brl';
+        $order->shipping_amount = 0;
+        $order->shipping_method = 'none';
+        $order->notes = 'Order placed by ' . auth()->user()->name;
+
+        $address = new Address();
+        $address->first_name = $this->first_name;
+        $address->last_name = $this->last_name;
+        $address->phone = $this->phone;
+        $address->street_address = $this->street_address;
+        $address->city = $this->city;
+        $address->state = $this->state;
+        $address->zip_code = $this->zip_code;
+
+        $redirect_url = '';
+
+        if($this->payment_method == 'stripe'){
+            Stripe::setApiKey(env('STRIPE_SECRET'));
+
+            $sessionCheckout = Session::create([
+                'payment_method_types' => ['card'],
+                'customer_email' => auth()->user()->email,
+                'line_items' => $lineItems,
+                'mode' => 'payment',
+                'success_url' => route('success') . '?session_id={CHECKOUT_SESSION_ID}',
+                'cancel_url' => route('cancel'),
+            ]);
+
+            $redirect_url = $sessionCheckout->url;
+        } else {
+            $redirect_url = route('success');
+        }
+
+        $order->save();
+        $order->address()->save($address);
+        $order->items()->createMany($cartItems);
+        CartManagement::clearCartItems();
+
+        return redirect($redirect_url);
+    }
+
     public function render()
     {
-        return view('livewire.checkout-page');
+        $cartItems = CartManagement::getCartItemsFromCookie();
+        $grandTotal = CartManagement::calculateGrandTotal($cartItems);
+
+        return view('livewire.checkout-page' , compact('cartItems', 'grandTotal'));
     }
 }
