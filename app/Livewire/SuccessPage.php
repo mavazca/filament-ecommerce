737d18407fa4533b7bef5a43640eb30316ed60be
Marcelo Vaz Castilho <marcelo.vaz.castilho@gmail.com>
2024-04-21 20:20:31 -0300

    feat: add envio de email pagina de sucesso e erro no checkout

--- app/Livewire/SuccessPage.php
+++ app/Livewire/SuccessPage.php
@@ -2,12 +2,52 @@
 
 namespace App\Livewire;
 
+use App\Models\Order;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
 use Livewire\Component;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 
+#[Title('Success Page - E-commerce')]
 class SuccessPage extends Component
 {
+    #[Url]
+    public $session_id;
+
+    protected $stripe;
+
+    public function __construct(Stripe $stripe)
+    {
+        $this->stripe = $stripe;
+    }
+
     public function render()
     {
-        return view('livewire.success-page');
+        $lastOrder = Order::with('address')->where('user_id', auth()->user()->id)->latest()->first();
+
+        if($this->session_id){
+            $this->handlePayment($lastOrder);
+        }
+
+        return view('livewire.success-page', [
+            'order' => $lastOrder
+        ]);
+    }
+
+    protected function handlePayment($order)
+    {
+        $this->stripe::setApiKey(env('STRIPE_SECRET'));
+        $session_info = Session::retrieve($this->session_id);
+
+        if($session_info->payment_status !== 'paid'){
+            $order->payment_status = 'failed';
+            $order->save();
+
+            return redirect()->route('cancel');
+        }
+
+        $order->payment_status = 'paid';
+        $order->save();
     }
 }
