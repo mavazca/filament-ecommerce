6f82adde20225e1cf6f72c75ac77d8e102d821ce
Marcelo Vaz Castilho <marcelo.vaz.castilho@gmail.com>
2024-04-23 21:16:24 -0300

    feat: add pagina meus pedidos e detalhe do pedido

--- app/Livewire/MyOrderDetailPage.php
+++ app/Livewire/MyOrderDetailPage.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire;
 
+use App\Models\Order;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Orders Detail Page - E-commerce')]
 class MyOrderDetailPage extends Component
 {
+    public $order;
+
+    public function mount($order_id)
+    {
+        $this->order = Order::with(['items.product', 'address'])
+            ->where('id', $order_id)
+            ->where('user_id', auth()->id())
+            ->firstOrFail();
+    }
+
     public function render()
     {
-        return view('livewire.my-order-detail-page');
+        return view('livewire.my-order-detail-page', [
+            'order' => $this->order
+        ]);
     }
 }
--- app/Livewire/MyOrdersPage.php
+++ app/Livewire/MyOrdersPage.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Order;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
+#[Title('My Orders Page - E-commerce')]
 class MyOrdersPage extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.my-orders-page');
+        $myOrders = Order::where('user_id', auth()->id())->latest()->paginate(5);
+
+        return view('livewire.my-orders-page', [
+            'orders' => $myOrders
+        ]);
     }
 }
